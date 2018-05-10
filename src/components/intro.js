d822d30dd6236fb888a22044d824a250f9806b91
Pedro Porto <mail@pedroporto.com.pt>
2018-05-10 11:47:11 +0100

    Intro Component.
    A little introduction to the app. No dynamic data here.

---
+++ src/components/intro.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+import reactLogo from '../assets/logos/react.svg';
+import fetchImage from '../assets/images/fetch.png';
+
+const imageStyle = {
+	width: '100%',
+	heigth: 'auto',
+}
+
+export default class ApiIntro extends Component {
+	render(){
+		return(
+			<div className="container app">
+				<div className="row">
+					<div className="col-md-12 text-center mb-4">
+						<h5>
+							Simple React APP
+						</h5>
+					</div>
+					<div className="col-md-6 mycontent-left">
+						<p>
+							This is a sample project as we are developing our skills in React integrating an API. For example, we could use the <a className="bold" href="https://developer.wordpress.org/rest-api/" target="_blank">WordPress Rest API</a>, but we are using the <a className="bold" href="https://jsonplaceholder.typicode.com/posts/1">JSON Placeholder API Project</a>.
+						</p>
+						<p>
+							JSON Placeholder is <span class="italic bold">a fake online REST API for testing and Prototyping</span> as they define themselves. If you want to know more about this project <a href="https://github.com/typicode/jsonplaceholder#how-to">read more here</a>.
+						</p>
+						<p>
+							We are also using <a className="bold" href="https://github.com/facebook/create-react-app">Create React App</a> as our boiler tempalte. <br />
+							Create React App is a great tool to build react apps with no build configuration and also a easy tool to use to start learning React JS. <br/>
+							Another great thing about this tool is that you don't need to install Babel or Webpack to start developing because they are preconfigured.
+						</p>
+					</div>
+					<div className="col-md-6">
+						<h6 className="bold">Here are some Documentation about the technogoly we are using here.</h6>
+						<div className="text-left">
+							<a className="btn btn-primary m-1" href="https://reactjs.org/docs/hello-world.html">
+								REACT JS
+							</a>
+							<a className="btn btn-primary m-1" href="https://github.com/axios/axios">
+								AXIOS
+							</a>
+							<a className="btn btn-primary m-1" href="https://getbootstrap.com/docs/4.0/getting-started/introduction/">
+								BOOTSTRAP 4
+							</a>
+							<a className="btn btn-primary m-1" href="https://github.com/facebook/create-react-app">
+								Create React App
+							</a>
+						</div>
+						<hr />
+						<div>
+							<h6 className="bold"> Here's our APP Fetch Flux </h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
