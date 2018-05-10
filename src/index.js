8e6e9413efa06fa070ed55ef159c80eb6fa3a32b
Pedro Porto <mail@pedroporto.com.pt>
2018-05-10 16:03:27 +0100

    Use of React Router.
    created class SimpleAPP. SimpleApp contains browser router and defines "where" are we on the APP.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Post from './Post';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class SimpleApp extends React.Component {
+	render(){
+		return(
+			<div>
+				<BrowserRouter>
+					<div>
+						<Route exact path="/" component={App} />
+						<Route exact path="/post" component={Post} />
+					</div>
+				</BrowserRouter>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<SimpleApp />, document.getElementById('root'));
 registerServiceWorker();
