598c797ed56c9d7acd1fa3199905c9fe971bd615
Pedro Porto <mail@pedroporto.com.pt>
2018-05-15 15:57:32 +0100

    added fetch component to route

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Post from './Post';
+import Fetch from './Fetch';
+
+import Nav from './components/nav';
+import Footer from './components/footer';
+
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -10,12 +15,15 @@ class SimpleApp extends React.Component {
 	render(){
 		return(
 			<div>
-				<BrowserRouter>
+				<Nav />
+				<BrowserRouter basename={'/simple-react-app'}>
 					<div>
 						<Route exact path="/" component={App} />
 						<Route exact path="/post" component={Post} />
+						<Route exact path="/fetch" component={Fetch} />
 					</div>
 				</BrowserRouter>
+				<Footer />
 			</div>
 		)
 	}
