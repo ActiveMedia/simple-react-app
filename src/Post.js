59a457098dd86d50a4977f6f1c909d74065655ab
Pedro Porto <mail@pedroporto.com.pt>
2018-05-15 15:58:35 +0100

    removed nav from component

--- src/Post.js
+++ src/Post.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
 // Components
-import Nav from './components/nav';
 import PostForm from './components/postForm';
-import Footer from './components/footer';
 
 class Post extends Component {
 	render() {
 		return(
 			<div>
-				<Nav/>
 				<PostForm />
-				<Footer />
 			</div>
 		)
 	}
