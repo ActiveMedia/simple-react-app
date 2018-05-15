708a183764f15b28ed409bb48a485eac6736c2b5
Pedro Porto <mail@pedroporto.com.pt>
2018-05-15 15:59:26 +0100

    added ./ into the Link. Soon will use React Router "Link"

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,16 +8,16 @@ export default class Nav extends Component {
 					<div className="container">
 						<div className="row">
 							<div className="col-md-3 p-3">
-								<a href="">Active Media React + API</a>
+								<a href="./">Active Media React + API</a>
 							</div>
 							<div className="col-md-9 text-right p-3">
 								<div className="btn-container pr-2">
-									<a href="/" className="btn btn-dark">
+									<a href="./fetch" className="btn btn-dark">
 										Fetch
 									</a>
 								</div>
 								<div className="btn-container pr-2">
-									<a href="/post" className="btn btn-dark">
+									<a href="./post" className="btn btn-dark">
 										Post
 									</a>
 								</div>
